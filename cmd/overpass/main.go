0258e0910f1ad200d8d253c170b3d2077167ba37
Dustin Pianalto <dustin@djpianalto.com>
2020-09-26 20:10:06 -0800

    initial test

--- cmd/overpass/main.go
+++ cmd/overpass/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/dustinpianalto/overpass/internal/twitter"
 )
 
 func main() {
 	tClient := twitter.Connect()
-	_, err := twitter.GetTimeline(tClient, 20)
-	if err != nil {
-		log.Fatal(err)
+	tweetChan, stream := twitter.StartUserScanner(tClient, "3347145623")
+	for tweet := range tweetChan {
+		log.Printf("%s\n%s - %s\n%s", tweet.IDStr, tweet.User.Name, tweet.User.IDStr, tweet.FullText)
 	}
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	log.Println(<-ch)
+
+	fmt.Println("Stopping Stream...")
+	stream.Stop()
 }
--- internal/twitter/twitter.go
+++ internal/twitter/twitter.go
@@ -1,7 +1,7 @@
 package twitter
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/dghubble/go-twitter/twitter"
 	oauth "github.com/dustinpianalto/overpass/internal/oauth2"
@@ -12,14 +12,33 @@ func Connect() *twitter.Client {
 	return twitter.NewClient(httpClient)
 }
 
-func GetTimeline(client *twitter.Client, count int) ([]twitter.Tweet, error) {
-	tweets, resp, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
-		Count: count,
-	})
-	if err != nil {
-		return nil, err
+func StartUserScanner(client *twitter.Client, userID string) (chan *twitter.Tweet, *twitter.Stream) {
+	demux := twitter.NewSwitchDemux()
+	tweetChan := make(chan *twitter.Tweet, 10)
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		tweetChan <- tweet
 	}
-	fmt.Println(resp)
-	fmt.Printf("%#v", tweets)
-	return tweets, nil
+	demux.StatusDeletion = func(deletion *twitter.StatusDeletion) {
+		log.Printf("%#v\n", deletion)
+	}
+	demux.StreamLimit = func(limit *twitter.StreamLimit) {
+		log.Printf("%#v\n", limit)
+	}
+	demux.StreamDisconnect = func(disconnect *twitter.StreamDisconnect) {
+		log.Printf("%#v\n", disconnect)
+	}
+	demux.Event = func(event *twitter.Event) {
+		log.Printf("%#v\n", event)
+	}
+	log.Printf("Starting Stream for %s\n", userID)
+	filterParams := &twitter.StreamFilterParams{
+		Follow:        []string{userID},
+		StallWarnings: twitter.Bool(true),
+	}
+	stream, err := client.Streams.Filter(filterParams)
+	if err != nil {
+		log.Println(err)
+	}
+	go demux.HandleChan(stream.Messages)
+	return tweetChan, stream
 }
